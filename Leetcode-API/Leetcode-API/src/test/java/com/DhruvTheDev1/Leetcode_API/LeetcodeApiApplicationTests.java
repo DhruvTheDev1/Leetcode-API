e93629f20ad2c177bb75cf688011ea419cfea7be
DhruvTheDev1 <dhruv2015@hotmail.co.uk>
2025-03-22 18:37:18 +0000

    Initial Commit - Springboot setup

---
+++ Leetcode-API/Leetcode-API/src/test/java/com/DhruvTheDev1/Leetcode_API/LeetcodeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DhruvTheDev1.Leetcode_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeetcodeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
