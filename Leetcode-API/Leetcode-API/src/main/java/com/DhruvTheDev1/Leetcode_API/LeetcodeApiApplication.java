a4c22ab6fa59e1592f8b41203e8c42aa1dfd00e3
DhruvTheDev1 <dhruv2015@hotmail.co.uk>
2025-03-25 15:16:08 +0000

    Building GraphQL service - GraphQL schema has top level query type - fields under are query operations exposed by application - Also defines the type - easy, medium, hard and total

--- Leetcode-API/Leetcode-API/src/main/java/com/DhruvTheDev1/Leetcode_API/LeetcodeApiApplication.java
+++ Leetcode-API/Leetcode-API/src/main/java/com/DhruvTheDev1/Leetcode_API/LeetcodeApiApplication.java
@@ -1,21 +1,11 @@
 package com.DhruvTheDev1.Leetcode_API;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
-@RestController
 public class LeetcodeApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LeetcodeApiApplication.class, args);
 	}
-
-	@GetMapping("/Leetcode")
-    public String leetcode() {
-      return "Hello";
-    }
 
 }
