3fe5d7af183062cfd4911aa1c2fde7e5d73717e4
DhruvTheDev1 <dhruv2015@hotmail.co.uk>
2025-03-26 16:49:43 +0000

    GraphQlTest slice test - Testing LeetcodeController

---
+++ Leetcode-API/Leetcode-API/src/test/java/com/DhruvTheDev1/Leetcode_API/LeetcodeControllerTests.java
@@ -0,0 +1,40 @@
+package com.DhruvTheDev1.Leetcode_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.graphql.GraphQlTest;
+import org.springframework.graphql.test.tester.GraphQlTester;
+
+@GraphQlTest(LeetcodeController.class)
+public class LeetcodeControllerTests {
+
+  @Autowired
+  private GraphQlTester graphQlTester;
+
+  @Test
+  void getLeetcodeStats() {
+    // graphql query
+    this.graphQlTester
+        .document("""
+                query GetLeetcodeStats($username: String!) {
+                    leetcodeStats(username: $username) {
+                        easy
+                        medium
+                        hard
+                        total
+                    }
+                }
+            """)
+        .variable("username", "TestUsername")
+        .execute() 
+        .path("leetcodeStats") 
+        .matchesJson("""
+                {
+                    "easy": 100,
+                    "medium": 100,
+                    "hard": 20,
+                    "total": 220
+                }
+            """);
+  }
+}
