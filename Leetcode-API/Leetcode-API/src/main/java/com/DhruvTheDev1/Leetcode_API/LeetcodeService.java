c7787128ce7c810a344e191b155cdbeb523ee05f
DhruvTheDev1 <dhruv2015@hotmail.co.uk>
2025-03-28 16:54:00 +0000

    Processing Data to POJO

---
+++ Leetcode-API/Leetcode-API/src/main/java/com/DhruvTheDev1/Leetcode_API/LeetcodeService.java
@@ -0,0 +1,79 @@
+package com.DhruvTheDev1.Leetcode_API;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Service
+public class LeetcodeService {
+
+  private final WebClient webClient;
+
+  // constructing webClient with GraphQL server base URL
+  public LeetcodeService(WebClient.Builder webClient) {
+    this.webClient = WebClient.builder()
+        .baseUrl("https://leetcode.com/graphql/")
+        .defaultHeader(HttpHeaders.CONTENT_TYPE, "application/json")
+        .build();
+  }
+
+  public LeetcodeData getStats() {
+    // query - POST request
+    String query = "{\r\n" + //
+        "    \"query\": \"query getUserProfile($username: String!) { matchedUser(username: $username) { submitStats { acSubmissionNum { difficulty count } } } }\",\r\n"
+        + //
+        "    \"variables\": {\r\n" + //
+        "        \"username\": \"\"\r\n" + //
+        "    }\r\n" + //
+        "}";
+
+    String response = webClient.post()
+        .bodyValue(query)
+        .retrieve()
+        .bodyToMono(String.class)
+        .block();
+
+        return convertToObjects(response);
+  }
+
+  public LeetcodeData convertToObjects(String response) {
+    JSONObject jsonObj = new JSONObject(response);
+
+    // getting acSubmissionNum array in response
+    JSONArray acSubmissionNum = jsonObj.getJSONObject("data")
+        .getJSONObject("matchedUser")
+        .getJSONObject("submitStats")
+        .getJSONArray("acSubmissionNum");
+
+    int easy = 0, medium = 0, hard = 0, total = 0;
+
+    for (int i = 0; i < acSubmissionNum.length(); i++) {
+      JSONObject stats = acSubmissionNum.getJSONObject(i);
+      String difficulty = stats.getString("difficulty");
+      int count = stats.getInt("count");
+
+      if (difficulty.equals("All")) {
+        total = count;
+      } else if (difficulty.equals("Easy")) {
+        easy = count;
+      } else if (difficulty.equals("Medium")) {
+        medium = count;
+      } else if (difficulty.equals("Hard")) {
+        hard = count;
+      }
+    }
+
+    return new LeetcodeData(easy, medium, hard, total);
+  }
+
+  // testing
+  public static void main(String[] args) {
+    LeetcodeService service = new LeetcodeService(WebClient.builder());
+    LeetcodeData stats = service.getStats();
+    System.out.println("Total: ");
+    System.out.println(stats.getTotal());
+    
+  }
+}
